0826dc6b02472071da1f991ca3b924725129bbf5
Sam Bangtson <bangtsons@wit.edu>
2025-02-05 13:33:02 -0500

    Improved the base project

--- js/main.js
+++ js/main.js
@@ -13,18 +13,16 @@ for (i = 0; i < myNodelist.length; i++) {
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
   }
 }
 
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
+var list = document.getElementById('myUL');
+list.addEventListener('click', function (ev) {
+  ev.target.classList.toggle('checked');
 }, false);
 
 // Create a new list item when clicking on the "Add" button
@@ -47,9 +45,18 @@ function newElement() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
     }
   }
 }
+
+// Create a new element when you hit enter in the text field
+var input = document.getElementById("myInput");
+var addButton = document.getElementById("myButton");
+input.addEventListener("keydown", function (e) {
+  if (e.code === "Enter") {
+    addButton.click();
+  }
+});
